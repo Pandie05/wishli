0b67aba476bb68b31402cb957f796b72b48d6e70
Pandie05 <hreyes@email.neit.edu>
2026-08-25 22:01:34 -0400

    temps styling and wishlist update

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -2,7 +2,6 @@ import { useState } from 'react'
 import type { FormEvent } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import { supabase } from '../lib/supabase'
-import GoogleButton from '../components/GoogleButton'
 
 const BAD_CREDENTIALS = 'Incorrect email/username or password.'
 
